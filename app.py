4ac04e4c773dbfcd5e695757f8d14d975da42395
Henrique <riquellopes@gmail.com>
2015-04-12 12:24:53 -0300

    adicionando ignore e api basica

--- app.py
+++ app.py
@@ -1,23 +1,46 @@
 # coding: utf-8
 import requests
-from flask import Flask
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config['SECRET_KEY'] = 'ldeyrweuy11yu2333uy3iu'
-app.config['DATA_BASE_URI'] = 'sqlite:///fb.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///fb.db'
+app.config['SQLALCHEMY_ECHO'] = False
 db = SQLAlchemy(app)
 
-@app.route("/<string:id>", methods=['GET'])
-def get(id):
-    return "1"
+
+class Person(db.Model):
+    __tablename__ = 'person'
+    username = db.Column(db.String(100))
+    facebookId = db.Column('facebookId', db.Integer, primary_key=True)
+    name = db.Column(db.String(60))
+    gender = db.Column(db.String(60))
 
 
-@app.route("/<string:id>", methods=['DELETE'])
+@app.route("/person/", methods=['POST'])
+def post():
+    response = requests.get("http://graph.facebook.com/{}".format(request.form.get('facebookid')))
+    json = response.json()
+    p = Person(**{
+        'username': json['username'],
+        'facebookId': json['id'],
+        'name': json['name'],
+        'gender': json['gender']
+    })
+    db.session.add(p)
+    db.session.commit()
+    return "", 201
+
+
+@app.route("/person/<string:id>", methods=['DELETE'])
 def delete(id):
-    return "2"
+    Person.query.get(id).query.delete()
+    db.session.commit()
+    return "", 204
 
 
 if __name__ == '__main__':
+    db.create_all()
     app.run()
